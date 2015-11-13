52419e4119fb21e0372f1136f8b379c543116076
Ketaki Rao <ketakisrao@outlook.com>
2015-11-13 19:27:38 +0530

    Django upgrade

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 import os
 
 # Put here required packages
-packages = ['Django<=1.6',]
+packages = ['Django<=1.8.4',]
 
 if 'REDISCLOUD_URL' in os.environ and 'REDISCLOUD_PORT' in os.environ and 'REDISCLOUD_PASSWORD' in os.environ:
      packages.append('django-redis-cache')
@@ -11,9 +11,9 @@ if 'REDISCLOUD_URL' in os.environ and 'REDISCLOUD_PORT' in os.environ and 'REDIS
 
 setup(name='YourAppName',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='Simply Res',
+      author='Ketaki Rao',
+      author_email='ketakisrao@outlook.com',
       url='https://pypi.python.org/pypi',
       install_requires=packages,
 )
