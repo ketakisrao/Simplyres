5467c46435a320161f1dac0c6b6a00995fb68a9f
Ketaki Rao <ketakisrao@outlook.com>
2015-11-17 05:31:29 +0530

    Checks for UI

--- setup.py
+++ setup.py
@@ -3,18 +3,17 @@ from setuptools import setup
 import os
 
 # Put here required packages
-packages = ['Django<=1.8.4',]
+packages = ['Django<=1.8.4', 'python-social-auth==0.2.1', 'django-crispy-forms', 'djangorestframework', 'django-registration-redux']
 
 if 'REDISCLOUD_URL' in os.environ and 'REDISCLOUD_PORT' in os.environ and 'REDISCLOUD_PASSWORD' in os.environ:
      packages.append('django-redis-cache')
      packages.append('hiredis')
 
-setup(name='YourAppName',
+setup(name='SimplyRes!',
       version='1.0',
-      description='Simply Res',
+      description='SimplyReserve!',
       author='Ketaki Rao',
       author_email='ketakisrao@outlook.com',
-      url='https://pypi.python.org/pypi',
+      url='https://simplyres-ketakisrao.rhcloud.com/',
       install_requires=packages,
 )
-
